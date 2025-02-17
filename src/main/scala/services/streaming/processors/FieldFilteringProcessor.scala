d8bed93ca92b81edb1b1035292596176e001e10e
Vitalii Savitskii <visa@ecco.com>
2025-02-17 14:33:52 +0100

    Minor fixes

--- src/main/scala/services/streaming/processors/FieldFilteringProcessor.scala
+++ src/main/scala/services/streaming/processors/FieldFilteringProcessor.scala
@@ -2,17 +2,26 @@ package com.sneaksanddata.arcane.framework
 package services.streaming.processors
 
 import models.DataRow
-import models.settings.{AllFields, ExcludeFields, FieldSelectionRule, IncludeFields}
+import models.settings.FieldSelectionRule.IncludeFields
+import models.settings.{FieldSelectionRuleSettings, FieldSelectionRule}
 import services.streaming.base.BatchProcessor
 
 import zio.Chunk
 import zio.stream.ZPipeline
 
-class FieldFilteringProcessor(fieldSelectionRule: FieldSelectionRule) extends BatchProcessor[DataRow, DataRow]:
+/**
+ * The batch processor implementation that filters the fields of a DataRow.
+ * @param fieldSelectionRule The field selection rule.
+ */
+class FieldFilteringProcessor(fieldSelectionRule: FieldSelectionRuleSettings) extends BatchProcessor[DataRow | Any, DataRow | Any]:
 
-  def process: ZPipeline[Any, Throwable, DataRow, DataRow] = ZPipeline.map { row =>
-    fieldSelectionRule match
-      case _: AllFields => row
-      case includeFields: IncludeFields => row.filter(entry => includeFields.fields.contains(entry.name))
-      case excludeFields: ExcludeFields => row.filter(entry => !excludeFields.fields.contains(entry.name))
+  def process: ZPipeline[Any, Throwable, DataRow|Any, DataRow|Any] = ZPipeline.map {
+    case row: DataRow => filterFields(row)
+    case other => other
   }
+
+  private def filterFields(row: DataRow) = fieldSelectionRule.rule match
+    case includeFields: FieldSelectionRule.IncludeFields => row.filter(entry => includeFields.fields.contains(entry.name))
+    case excludeFields: FieldSelectionRule.ExcludeFields => row.filter(entry => !excludeFields.fields.contains(entry.name))
+    case _ => row
+
