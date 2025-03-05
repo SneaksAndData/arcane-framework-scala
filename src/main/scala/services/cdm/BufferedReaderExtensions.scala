5c5676dd24ca422fd63988bac446c1ab73e59f40
Vitalii Savitskii <visa@ecco.com>
2025-03-05 16:21:31 +0100

    Add the multiline CSV reader stream. (#66)
    Part of #29
    
    Some code cleanup.

--- src/main/scala/services/cdm/BufferedReaderExtensions.scala
+++ src/main/scala/services/cdm/BufferedReaderExtensions.scala
@@ -2,17 +2,29 @@ package com.sneaksanddata.arcane.framework
 package services.cdm
 
 import zio.ZIO
+import zio.stream.ZStream
 
 import java.io.BufferedReader
 
 object BufferedReaderExtensions:
+
+
+  /**
+   * Reads a CSV file line by line, concatenating lines that are split by newlines within a quoted field.
+   * @param javaReader The BufferedReader to read from.
+   *                   The reader should be closed after the stream is consumed.
+   * @return A ZStream effect that represents the next line of the CSV file.
+   */
+  extension (javaReader: BufferedReader) def readMultilineCSV: ZStream[Any, Throwable, String] =
+    ZStream.repeatZIO(javaReader.readMultilineCsvLine)
+      .collectWhile { case Some(line) => line }
 
   /**
    * Reads a CSV file line by line, concatenating lines that are split by newlines within a quoted field.
    * @param stream The BufferedReader to read from.
    * @return A ZIO effect that represents the next line of the CSV file.
    */
-  extension (stream: BufferedReader) def readMultilineCsv: ZIO[Any, Throwable, Option[String]] =
+  extension (stream: BufferedReader) def readMultilineCsvLine: ZIO[Any, Throwable, Option[String]] =
     for {
       dataLine <- ZIO.attemptBlocking(Option(stream.readLine()))
       continuation <- tryGetContinuation(stream, dataLine.getOrElse("").count(_ == '"'), new StringBuilder())
