2ff26d7bf0b477b7c61abc759ed289aae9b18914
Vitalii Savitskii <visa@ecco.com>
2025-03-07 13:33:47 +0100

    Memoize data in schema provider

--- src/test/scala/services/cdm/CmdSchemaProviderTests.scala
+++ src/test/scala/services/cdm/CmdSchemaProviderTests.scala
@@ -4,12 +4,19 @@ package services.cdm
 import services.storage.services.AzureBlobStorageReader
 
 import com.azure.storage.common.StorageSharedKeyCredential
+import com.sneaksanddata.arcane.framework.services.storage.base.BlobStorageReader
+import com.sneaksanddata.arcane.framework.services.storage.models.azure.AdlsStoragePath
+import org.easymock.EasyMock.{replay, verify}
+import org.easymock.{EasyMock, IAnswer}
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.should
-import zio.{Runtime, Unsafe}
+import org.scalatestplus.easymock.EasyMockSugar
+import zio.{Runtime, Task, Unsafe, ZIO}
 
-class CmdSchemaProviderTests extends AsyncFlatSpec with Matchers:
+import java.io.{BufferedReader, StringReader}
+
+class CmdSchemaProviderTests extends AsyncFlatSpec with Matchers with EasyMockSugar:
   private val runtime = Runtime.default
 
   private val endpoint = "http://localhost:10001/devstoreaccount1"
@@ -28,5 +35,26 @@ class CmdSchemaProviderTests extends AsyncFlatSpec with Matchers:
 
     Unsafe.unsafe(implicit unsafe => runtime.unsafe.runToFuture(provider.getSchema)).map { result =>
       result.length should be >= 1
+    }
+  }
+
+  it should "memoize schema calls" in {
+
+    val path = s"abfss://$container@$storageAccount.dfs.core.windows.net/"
+    val storageReaderMock = mock[BlobStorageReader[AdlsStoragePath]]
+    expecting {
+      storageReaderMock.streamBlobContent(EasyMock.anyObject()).andAnswer(() => {
+        val arg = EasyMock.getCurrentArgument[AdlsStoragePath](0)
+        storageReader.streamBlobContent(arg)
+      })
+      .once()
+    }
+    replay(storageReaderMock)
+    val provider = CdmSchemaProvider(storageReaderMock, path, tableName)
+
+    val task = provider.getSchema *> provider.getSchema *> provider.getSchema *> provider.getSchema *> provider.getSchema
+
+    Unsafe.unsafe(implicit unsafe => runtime.unsafe.runToFuture(task)).map { result =>
+        noException should be thrownBy verify(storageReaderMock)
     }
   }
