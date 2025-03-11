4b5bf1ca4231f4dec09ef533baaf9e846d1d1ce7
Vitalii Savitskii <visa@ecco.com>
2025-03-11 13:13:07 +0100

    Add javadoc

--- src/main/scala/services/app/GenericStreamRunnerService.scala
+++ src/main/scala/services/app/GenericStreamRunnerService.scala
@@ -8,29 +8,60 @@ import services.streaming.base.{MetadataEnrichedRowStreamElement, StreamingGraph
 import zio.stream.{ZPipeline, ZSink}
 import zio.{Tag, ZIO, ZLayer}
 
-class GenericStreamRunnerService(builder: StreamingGraphBuilder, lifetimeService: StreamLifetimeService)
-  extends StreamRunnerService:
+/**
+ * A service that can be used to run a stream.
+ *
+ * @param builder The stream graph builder.
+ * @param lifetimeService The stream lifetime service.
+ */
+class GenericStreamRunnerService(builder: StreamingGraphBuilder, lifetimeService: StreamLifetimeService) extends StreamRunnerService:
 
+  /**
+   * Runs the stream.
+   *
+   * @return A ZIO effect that represents the stream.
+   */
   def run: ZIO[Any, Throwable, Unit] =
     lifetimeService.start()
     builder.produce.via(streamLifetimeGuard).run(logResults)
 
-
+  /**
+   * The stage that completes the stream until the lifetime service is cancelled.
+   */
   private def streamLifetimeGuard = ZPipeline.takeUntil(_ => lifetimeService.cancelled)
 
+  /**
+   * Logs the results of the stream.
+   */
   private def logResults = ZSink.foreach(result => zlog(s"Processing completed: $result"))
 
+/**
+ * The companion object for the StreamRunnerServiceImpl class.
+ */
 object GenericStreamRunnerService:
 
-    type Environment = StreamLifetimeService & StreamingGraphBuilder
+  /**
+   * The required environment for the GenericStreamRunnerService.
+   */
+  type Environment = StreamLifetimeService & StreamingGraphBuilder
 
-    def apply(builder: StreamingGraphBuilder, lifetimeService: StreamLifetimeService): GenericStreamRunnerService =
-      new GenericStreamRunnerService(builder, lifetimeService)
+  /**
+   * Creates a new instance of the GenericStreamRunnerService class.
+   *
+   * @param builder The stream graph builder.
+   * @param lifetimeService The stream lifetime service.
+   * @return A new instance of the GenericStreamRunnerService class.
+   */
+  def apply(builder: StreamingGraphBuilder, lifetimeService: StreamLifetimeService): GenericStreamRunnerService =
+    new GenericStreamRunnerService(builder, lifetimeService)
 
-    val layer: ZLayer[Environment, Nothing, GenericStreamRunnerService] =
-      ZLayer{
-        for
-          lifetimeService <- ZIO.service[StreamLifetimeService]
-          builder <- ZIO.service[StreamingGraphBuilder]
-        yield GenericStreamRunnerService(builder, lifetimeService)
-      }
+  /**
+   * The ZLayer for the GenericStreamRunnerService.
+   */
+  val layer: ZLayer[Environment, Nothing, StreamRunnerService] =
+    ZLayer{
+      for
+        lifetimeService <- ZIO.service[StreamLifetimeService]
+        builder <- ZIO.service[StreamingGraphBuilder]
+      yield GenericStreamRunnerService(builder, lifetimeService)
+    }
