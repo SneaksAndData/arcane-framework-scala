96eefd0b07f72b98407c8b7de228ecd1b45f391e
Vitalii Savitskii <visa@ecco.com>
2025-03-18 10:25:03 +0100

    Fix

--- src/main/scala/services/streaming/processors/transformers/StagingProcessor.scala
+++ src/main/scala/services/streaming/processors/transformers/StagingProcessor.scala
@@ -61,7 +61,7 @@ class StagingProcessor(stagingDataSettings: StagingDataSettings,
       newSchema <- tableManager.getSchema(stagingDataSettings.getStagingTableName)
       ordering = newSchema.map(_.name).zipWithIndex.toMap
 
-      table <- catalogWriter.append(rows.map(r => r.sortBy(c => ordering(c.name.toUpperCase()))), stagingDataSettings.getStagingTableName, arcaneSchema)
+      table <- catalogWriter.append(rows.map(r => r.sortBy(c => ordering(c.name.toUpperCase()))), stagingDataSettings.getStagingTableName, newSchema)
         .tapErrorCause(cause => zlog("Error writing data to staging table: {cause}", cause))
         .retry(retryPolicy)
 
