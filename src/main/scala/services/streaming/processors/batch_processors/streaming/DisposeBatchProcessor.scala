be6e69486fac947bda9b6c568139a996e2b0932f
Vitalii Savitskii <visa@ecco.com>
2025-03-19 13:25:50 +0100

    Skip dispose of the staged batches in backfill mode

--- src/main/scala/services/streaming/processors/batch_processors/streaming/DisposeBatchProcessor.scala
+++ src/main/scala/services/streaming/processors/batch_processors/streaming/DisposeBatchProcessor.scala
@@ -5,13 +5,14 @@ import logging.ZIOLogAnnotations.*
 import services.base.DisposeServiceClient
 import services.streaming.base.StagedBatchProcessor
 
+import com.sneaksanddata.arcane.framework.models.app.StreamContext
 import zio.stream.ZPipeline
 import zio.{ZIO, ZLayer}
 
 /**
  * Processor that merges data into a target table.
  */
-class DisposeBatchProcessor(disposeServiceClient: DisposeServiceClient)
+class DisposeBatchProcessor(disposeServiceClient: DisposeServiceClient, streamContext: StreamContext)
   extends StagedBatchProcessor:
 
   /**
@@ -21,9 +22,13 @@ class DisposeBatchProcessor(disposeServiceClient: DisposeServiceClient)
    */
   override def process: ZPipeline[Any, Throwable, BatchType, BatchType] =
     ZPipeline.mapZIO(batchesSet =>
-      for _ <- zlog(s"Disposing batch set with index ${batchesSet.batchIndex}")
-          _ <- ZIO.foreach(batchesSet.groupedBySchema)(batch => disposeServiceClient.disposeBatch(batch))
-      yield batchesSet
+      if streamContext.IsBackfilling then
+        for _ <- zlog(s"Running in backfill mode. Skipping dispose of batch set with index ${batchesSet.batchIndex}")
+        yield batchesSet
+      else  
+        for _ <- zlog(s"Disposing batch set with index ${batchesSet.batchIndex}")
+            _ <- ZIO.foreach(batchesSet.groupedBySchema)(batch => disposeServiceClient.disposeBatch(batch))
+        yield batchesSet
     )
 
 object DisposeBatchProcessor:
@@ -34,13 +39,14 @@ object DisposeBatchProcessor:
    * @param DisposeServiceClient The JDBC consumer.
    * @return The initialized MergeProcessor instance
    */
-  def apply(DisposeServiceClient: DisposeServiceClient): DisposeBatchProcessor =
-    new DisposeBatchProcessor(DisposeServiceClient)
+  def apply(DisposeServiceClient: DisposeServiceClient, streamContext: StreamContext): DisposeBatchProcessor =
+    new DisposeBatchProcessor(DisposeServiceClient, streamContext)
 
   /**
    * The required environment for the MergeBatchProcessor.
    */
   type Environment = DisposeServiceClient
+    & StreamContext
 
   /**
    * The ZLayer that creates the MergeProcessor.
@@ -49,5 +55,6 @@ object DisposeBatchProcessor:
     ZLayer {
       for
         disposeServiceClient <- ZIO.service[DisposeServiceClient]
-      yield DisposeBatchProcessor(disposeServiceClient)
+        streamContext <- ZIO.service[StreamContext]
+      yield DisposeBatchProcessor(disposeServiceClient, streamContext)
     }
