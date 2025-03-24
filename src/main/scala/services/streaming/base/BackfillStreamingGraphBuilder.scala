3c816445e1a7186a44b7a09cd7f22c9b0fa8d0d2
Vitalii Savitskii <visa@ecco.com>
2025-03-24 16:22:42 +0100

    Add the graph builders for backfill streams
    This PR adds the stream graph builders for both `merge` and `overwrite` behaviors in backfill mode.

---
+++ src/main/scala/services/streaming/base/BackfillStreamingGraphBuilder.scala
@@ -0,0 +1,10 @@
+
+package com.sneaksanddata.arcane.framework
+package services.streaming.base
+
+import zio.stream.ZStream
+
+/**
+ * A trait that represents a stream graph builder for backfilling.
+ */
+trait BackfillStreamingGraphBuilder extends StreamingGraphBuilder
---
+++ src/main/scala/services/streaming/base/BackfillStreamingMergeDataProvider.scala
@@ -0,0 +1,16 @@
+package com.sneaksanddata.arcane.framework
+package services.streaming.base
+
+import zio.Task
+
+/**
+ * A trait that represents a backfill data provider.
+ */
+trait BackfillStreamingMergeDataProvider:
+
+  /**
+   * Provides the backfill data.
+   *
+   * @return A task that represents the backfill data.
+   */
+  def requestBackfill: Task[Unit]
