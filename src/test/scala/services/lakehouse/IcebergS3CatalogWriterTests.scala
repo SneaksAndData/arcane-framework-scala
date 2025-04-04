08564099ebc66fc61e84d404d0e016f5df4526d8
Vitalii Savitskii <visa@ecco.com>
2025-04-04 11:22:35 +0200

    WIP

--- src/test/scala/services/lakehouse/IcebergS3CatalogWriterTests.scala
+++ src/test/scala/services/lakehouse/IcebergS3CatalogWriterTests.scala
@@ -29,15 +29,15 @@ class IcebergS3CatalogWriterTests extends flatspec.AsyncFlatSpec with Matchers:
     override val s3CatalogFileIO: S3CatalogFileIO = S3CatalogFileIO
     override val stagingLocation: Option[String] = Some("s3://tmp/polaris/test")
 
-  private val schema = Seq(MergeKeyField, Field(name = "colA", fieldType = IntType), Field(name = "colB", fieldType = StringType), Field(name = "colC", fieldType = ByteArrayType), Field(name = "colD", fieldType = TimestampType))
+  private val schema = Seq(MergeKeyField, Field(name = "colA", fieldType = IntType), Field(name = "colB", fieldType = StringType))
   private val writer: CatalogWriter[RESTCatalog, Table, Schema] = IcebergS3CatalogWriter(settings, IdentityIcebergDataRowConverter())
 
   it should "create a table when provided schema and rows" in {
     val rows = Seq(
-      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key1"), DataCell(name = "colA", Type = IntType, value = 1), DataCell(name = "colB", Type = StringType, value = "abc"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
-      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key2"), DataCell(name = "colA", Type = IntType, value = 2), DataCell(name = "colB", Type = StringType, value = "def"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
-      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key3"), DataCell(name = "colA", Type = IntType, value = 2), DataCell(name = "colB", Type = StringType, value = "iop"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
-      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key4"), DataCell(name = "colA", Type = IntType, value = 3), DataCell(name = "colB", Type = StringType, value = "tyr"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
+      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key1"), DataCell(name = "colA", Type = IntType, value = 1), DataCell(name = "colB", Type = StringType, value = "abc")),
+      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key2"), DataCell(name = "colA", Type = IntType, value = 2), DataCell(name = "colB", Type = StringType, value = "def")),
+      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key3"), DataCell(name = "colA", Type = IntType, value = 2), DataCell(name = "colB", Type = StringType, value = "iop")),
+      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key4"), DataCell(name = "colA", Type = IntType, value = 3), DataCell(name = "colB", Type = StringType, value = "tyr"))
     )
 
     val task =  writer.write(
@@ -95,3 +95,23 @@ class IcebergS3CatalogWriterTests extends flatspec.AsyncFlatSpec with Matchers:
     
     Unsafe.unsafe(implicit unsafe => runtime.unsafe.runToFuture(task))
   }
+
+//  it should "create a table when provided schema and rows" in {
+//    val schema = Seq(MergeKeyField, Field(name = "colA", fieldType = IntType), Field(name = "colB", fieldType = StringType), Field(name = "colC", fieldType = ByteArrayType), Field(name = "colD", fieldType = TimestampType))
+//    val writer: CatalogWriter[RESTCatalog, Table, Schema] = IcebergS3CatalogWriter(settings, IdentityIcebergDataRowConverter())
+//
+//    val rows = Seq(
+//      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key1"), DataCell(name = "colA", Type = IntType, value = 1), DataCell(name = "colB", Type = StringType, value = "abc"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
+//      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key2"), DataCell(name = "colA", Type = IntType, value = 2), DataCell(name = "colB", Type = StringType, value = "def"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
+//      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key3"), DataCell(name = "colA", Type = IntType, value = 2), DataCell(name = "colB", Type = StringType, value = "iop"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
+//      List(DataCell(name = MergeKeyField.name, Type = MergeKeyField.fieldType, value = "key4"), DataCell(name = "colA", Type = IntType, value = 3), DataCell(name = "colB", Type = StringType, value = "tyr"), DataCell(name = "colC", Type = ByteArrayType, value = Array[Byte](1, 2, 3, 4, 5)), DataCell(name = "colD", Type = TimestampType, value = java.sql.Timestamp(System.currentTimeMillis()))),
+//    )
+//
+//    val task =  writer.write(
+//      data = rows,
+//      name = UUID.randomUUID.toString,
+//      schema = schema
+//    ).map(tbl => tbl.history().asScala.isEmpty should equal(false))
+//
+//    Unsafe.unsafe(implicit unsafe => runtime.unsafe.runToFuture(task))
+//  }
