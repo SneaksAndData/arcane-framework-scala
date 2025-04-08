c9ca3d28230392b7308b0043703d150e527719c4
George Zubrienko <gzu@ecco.com>
2025-04-08 13:08:05 +0200

    WIP

--- src/test/scala/tests/synapse/SynapseLinkReaderTests.scala
+++ src/test/scala/tests/synapse/SynapseLinkReaderTests.scala
@@ -7,7 +7,7 @@ import services.storage.services.AzureBlobStorageReader
 import services.synapse.base.SynapseLinkReader
 import tests.synapse.SynapseAzureBlobReaderExtensionsTests.{container, storageAccount}
 
-import zio.{Scope, ZIO}
+import zio.{Scope, ZIO, Cause}
 import zio.test.*
 import zio.test.TestAspect.timeout
 
@@ -16,6 +16,7 @@ import java.time.{Duration, OffsetDateTime}
 object SynapseLinkReaderTests extends ZIOSpecDefault:
   private val endpoint = "http://localhost:10001/devstoreaccount1"
   private val container = "cdm-e2e"
+  private val malformedSchemaContainer = "cdm-e2e-malformed-schema"
   private val storageAccount = "devstoreaccount1"
   private val accessKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
   private val tableName = "dimensionattributelevelvalue"
@@ -42,5 +43,13 @@ object SynapseLinkReaderTests extends ZIOSpecDefault:
         schema <- synapseLinkReader.getSchema
       // 25 fields plus ARCANE_MERGE_KEY  
       yield assertTrue(schema.size == 26)
+    },
+
+    test("fails on incorrect schema") {
+      for
+        path <- ZIO.succeed(AdlsStoragePath(s"abfss://$malformedSchemaContainer@$storageAccount.dfs.core.windows.net/").get)
+        synapseLinkReader <- ZIO.succeed(SynapseLinkReader(storageReader, tableName, path))
+        exit <- synapseLinkReader.getChanges(OffsetDateTime.now().minus(Duration.ofHours(12))).map(_ => 1).runSum.exit
+      yield assertTrue(exit.is(_.die).getMessage.startsWith("Unable to parse model.json file under location"))
     }
   ) @@ timeout(zio.Duration.fromSeconds(10)) @@ TestAspect.withLiveClock
