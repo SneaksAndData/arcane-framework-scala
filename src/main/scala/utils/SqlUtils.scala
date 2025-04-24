99905233ed2f4c32d752993feb8d0cf6dd35a931
Vitalii Savitskii <visa@ecco.com>
2025-04-24 13:44:21 +0200

    Add longvarchar type (#156)

--- src/main/scala/utils/SqlUtils.scala
+++ src/main/scala/utils/SqlUtils.scala
@@ -46,9 +46,15 @@ object SqlUtils:
     case java.sql.Types.TIMESTAMP => Success(ArcaneType.TimestampType)
     case java.sql.Types.TIMESTAMP_WITH_TIMEZONE => Success(ArcaneType.DateTimeOffsetType)
     case java.sql.Types.DECIMAL => Success(ArcaneType.BigDecimalType(precision, scale))
+
     // numeric is functionally identical to decimal
     // see: https://learn.microsoft.com/en-us/sql/t-sql/data-types/decimal-and-numeric-transact-sql
     case java.sql.Types.NUMERIC => Success(ArcaneType.BigDecimalType(precision, scale))
+
+    // The SQL Server text and ntext types map to the JDBC LONGVARCHAR and LONGNVARCHAR type, respectively.
+    // see: https://learn.microsoft.com/en-us/sql/connect/jdbc/understanding-data-type-differences?view=sql-server-ver16#character-types
+    case java.sql.Types.LONGVARCHAR => Success(ArcaneType.StringType)
+    case java.sql.Types.LONGNVARCHAR => Success(ArcaneType.StringType)
     case java.sql.Types.DOUBLE => Success(ArcaneType.DoubleType)
     case java.sql.Types.INTEGER => Success(ArcaneType.IntType)
     case java.sql.Types.FLOAT => Success(ArcaneType.FloatType)
