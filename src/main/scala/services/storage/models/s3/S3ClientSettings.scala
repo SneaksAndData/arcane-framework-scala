8c33bd415fe9060b069cc1ef889bd4e93d9b0b98
George Zubrienko <gzu@ecco.com>
2025-07-07 14:24:53 +0200

    WIP S3 code

--- src/main/scala/services/storage/models/s3/S3ClientSettings.scala
+++ src/main/scala/services/storage/models/s3/S3ClientSettings.scala
@@ -4,12 +4,16 @@ package services.storage.models.s3
 import software.amazon.awssdk.regions.Region
 
 import java.net.URI
+import java.time.Duration
 
 case class S3ClientSettings(
   usePathStyle: Boolean,
   region: Option[Region],
   endpoint: Option[URI],
-  maxResultsPerPage: Int
+  maxResultsPerPage: Int,
+    retryMaxAttempts: Int,
+    retryBaseDelay: Duration,
+retryMaxDelay: Duration
 )
 
 object S3ClientSettings:
@@ -17,10 +21,16 @@ object S3ClientSettings:
       region: Option[String] = None,
       endpoint: Option[String] = None,
       pathStyleAccess: Boolean = false,
-        maxResultsPerPage: Int = 1000
+        maxResultsPerPage: Int = 1000,
+      retryMaxAttempts: Int = 5,
+        retryBaseDelay: Duration = Duration.ofMillis(100),
+      retryMaxDelay: Duration = Duration.ofSeconds(1)
   ): S3ClientSettings = new S3ClientSettings(
     region = region.map(Region.of),
     endpoint = endpoint.map(URI.create),
     usePathStyle = pathStyleAccess,
-    maxResultsPerPage = maxResultsPerPage
+    maxResultsPerPage = maxResultsPerPage,
+    retryMaxAttempts = retryMaxAttempts,
+    retryBaseDelay = retryBaseDelay, 
+    retryMaxDelay = retryMaxDelay
   )
--- src/main/scala/services/storage/services/s3/S3BlobStorageReader.scala
+++ src/main/scala/services/storage/services/s3/S3BlobStorageReader.scala
@@ -6,6 +6,7 @@ import services.storage.models.s3.{S3ClientSettings, S3StoragePath}
 import com.sneaksanddata.arcane.framework.services.storage.base.BlobStorageReader
 import com.sneaksanddata.arcane.framework.services.storage.models.base.{BlobPath, StoredBlob}
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.awscore.retry.AwsRetryStrategy
 import software.amazon.awssdk.services.s3.S3Client
 import zio.Task
 import zio.stream.ZStream
@@ -15,15 +16,16 @@ import java.io.BufferedReader
 final class S3BlobStorageReader(settings: Option[S3ClientSettings]) extends BlobStorageReader[S3StoragePath]:
   private val serviceClientSettings = settings.getOrElse(S3ClientSettings())
   private val defaultCredential = DefaultCredentialsProvider.builder().asyncCredentialUpdateEnabled(true).build()
+  private val retryStrategy = AwsRetryStrategy.standardRetryStrategy().toBuilder.maxAttempts(serviceClientSettings.retryMaxAttempts)
   private val s3Client = {
     var builder = S3Client.builder().credentialsProvider(defaultCredential).forcePathStyle(serviceClientSettings.usePathStyle).crossRegionAccessEnabled(true).dualstackEnabled(true)
-    
-    if (serviceClientSettings.region.isDefined) 
+
+    if (serviceClientSettings.region.isDefined)
       builder = builder.region(serviceClientSettings.region.get)
-      
+
     if (serviceClientSettings.endpoint.isDefined)
       builder = builder.endpointOverride(serviceClientSettings.endpoint.get)
-    
+
     builder.build()
   }
 
