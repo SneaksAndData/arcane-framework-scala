44a35981059331f046631ce973668000d080c3fb
George Zubrienko <gzu@ecco.com>
2025-07-07 14:36:27 +0200

    WIP S3 code

--- src/main/scala/services/storage/services/s3/S3BlobStorageReader.scala
+++ src/main/scala/services/storage/services/s3/S3BlobStorageReader.scala
@@ -7,8 +7,10 @@ import com.sneaksanddata.arcane.framework.services.storage.base.BlobStorageReade
 import com.sneaksanddata.arcane.framework.services.storage.models.base.{BlobPath, StoredBlob}
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
 import software.amazon.awssdk.awscore.retry.AwsRetryStrategy
+import software.amazon.awssdk.retries.api.BackoffStrategy
 import software.amazon.awssdk.services.s3.S3Client
-import zio.Task
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest
+import zio.{Task, ZIO}
 import zio.stream.ZStream
 
 import java.io.BufferedReader
@@ -17,14 +19,23 @@ final class S3BlobStorageReader(settings: Option[S3ClientSettings]) extends Blob
   private val serviceClientSettings = settings.getOrElse(S3ClientSettings())
   private val defaultCredential     = DefaultCredentialsProvider.builder().asyncCredentialUpdateEnabled(true).build()
   private val retryStrategy =
-    AwsRetryStrategy.standardRetryStrategy().toBuilder.maxAttempts(serviceClientSettings.retryMaxAttempts)
-  private val s3Client = {
+    AwsRetryStrategy
+      .standardRetryStrategy()
+      .toBuilder
+      .maxAttempts(serviceClientSettings.retryMaxAttempts)
+      .backoffStrategy(
+        BackoffStrategy.exponentialDelay(serviceClientSettings.retryBaseDelay, serviceClientSettings.retryMaxDelay)
+      )
+      .circuitBreakerEnabled(true)
+      .build()
+  private val s3Client: S3Client = {
     var builder = S3Client
       .builder()
       .credentialsProvider(defaultCredential)
       .forcePathStyle(serviceClientSettings.usePathStyle)
       .crossRegionAccessEnabled(true)
       .dualstackEnabled(true)
+      .overrideConfiguration(o => o.retryStrategy(retryStrategy))
 
     if (serviceClientSettings.region.isDefined)
       builder = builder.region(serviceClientSettings.region.get)
@@ -35,10 +46,15 @@ final class S3BlobStorageReader(settings: Option[S3ClientSettings]) extends Blob
     builder.build()
   }
 
-  override def blobExists(blobPath: S3StoragePath): Task[Boolean] = ???
+  override def blobExists(blobPath: S3StoragePath): Task[Boolean] = ZIO
+    .attemptBlocking(
+      s3Client.headObject(HeadObjectRequest.builder().bucket(blobPath.bucket).key(blobPath.objectKey).build())
+    )
+    .flatMap(result => ZIO.logDebug(s"Blob ${blobPath.toHdfsPath} exists: ${result.eTag()}") *> ZIO.succeed(true))
+    .onError(_ => ZIO.logDebug(s"Blob ${blobPath.toHdfsPath} does not exist") *> ZIO.succeed(false))
 
-  override def readBlobContent(blobPath: BlobPath): Task[String] = ???
+  override def readBlobContent(blobPath: S3StoragePath): Task[String] = ???
 
-  override def streamBlobContent(blobPath: BlobPath): Task[BufferedReader] = ???
+  override def streamBlobContent(blobPath: S3StoragePath): Task[BufferedReader] = ???
 
   override def streamPrefixes(rootPrefix: S3StoragePath): ZStream[Any, Throwable, StoredBlob] = ???
