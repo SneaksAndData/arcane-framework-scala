528e9f47ccbb826431ca92a6771eb02b93c39f0e
George Zubrienko <gzu@ecco.com>
2025-07-07 16:43:41 +0200

    WIP S3 code

--- src/main/scala/services/storage/services/s3/S3BlobStorageReader.scala
+++ src/main/scala/services/storage/services/s3/S3BlobStorageReader.scala
@@ -7,11 +7,21 @@ import services.storage.models.base.StoredBlob
 import services.storage.models.s3.S3ModelConversions.given
 import services.storage.models.s3.{S3ClientSettings, S3StoragePath}
 
-import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, AwsCredentials, DefaultCredentialsProvider, StaticCredentialsProvider}
+import software.amazon.awssdk.auth.credentials.{
+  AwsBasicCredentials,
+  AwsCredentials,
+  DefaultCredentialsProvider,
+  StaticCredentialsProvider
+}
 import software.amazon.awssdk.awscore.retry.AwsRetryStrategy
 import software.amazon.awssdk.retries.api.BackoffStrategy
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.{GetObjectRequest, HeadObjectRequest, ListObjectsV2Request, NoSuchKeyException}
+import software.amazon.awssdk.services.s3.model.{
+  GetObjectRequest,
+  HeadObjectRequest,
+  ListObjectsV2Request,
+  NoSuchKeyException
+}
 import zio.stream.ZStream
 import zio.{Task, ZIO}
 
@@ -61,9 +71,8 @@ final class S3BlobStorageReader(
       s3Client.headObject(HeadObjectRequest.builder().bucket(blobPath.bucket).key(blobPath.objectKey).build())
     )
     .flatMap(result => ZIO.logDebug(s"Blob ${blobPath.toHdfsPath} exists: ${result.eTag()}") *> ZIO.succeed(true))
-    .catchSome {
-      case _: NoSuchKeyException =>
-        ZIO.logDebug(s"Blob ${blobPath.toHdfsPath} does not exist") *> ZIO.succeed(false)
+    .catchSome { case _: NoSuchKeyException =>
+      ZIO.logDebug(s"Blob ${blobPath.toHdfsPath} does not exist") *> ZIO.succeed(false)
     }
 
   override def readBlobContent(blobPath: S3StoragePath): Task[String] = for
@@ -105,7 +114,7 @@ final class S3BlobStorageReader(
                 .bucket(rootPrefix.bucket)
                 .prefix(rootPrefix.objectKey)
                 .maxKeys(serviceClientSettings.maxResultsPerPage)
-                .continuationToken(response.continuationToken())
+                .continuationToken(response.nextContinuationToken())
                 .build()
             )
           )
--- src/test/scala/tests/s3/S3BlobStorageReaderTests.scala
+++ src/test/scala/tests/s3/S3BlobStorageReaderTests.scala
@@ -10,17 +10,23 @@ import zio.{Scope, ZIO}
 object S3BlobStorageReaderTests extends ZIOSpecDefault {
   val bucket = "s3-blob-reader"
   override def spec: Spec[TestEnvironment & Scope, Any] = suite("S3BlobStorageReader")(
-    test(s"blobExists returns true if a blob exists in a bucket") {
+    test("blobExists returns true if a blob exists in a bucket") {
       for
         path   <- ZIO.succeed(S3StoragePath(s"s3a://$bucket/0.parquet.gzip").get)
         result <- storageReader.blobExists(path)
       yield assertTrue(result)
     },
-    test(s"blobExists returns false and doesn't error if a blob does not exist in a bucket") {
+    test("blobExists returns false and doesn't error if a blob does not exist in a bucket") {
       for
         path   <- ZIO.succeed(S3StoragePath(s"s3a://$bucket/0.parquet.zip").get)
         result <- storageReader.blobExists(path)
       yield assertTrue(!result)
-    }    
+    },
+    test("streamPrefixes returns a stream of correct length") {
+      for
+        path     <- ZIO.succeed(S3StoragePath(s"s3a://$bucket").get)
+        prefixes <- storageReader.streamPrefixes(path).runCount
+      yield assertTrue(prefixes == 50)
+    }
   )
 }
