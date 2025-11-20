c404cb4e8742a7ce9554a449ca29cb42d816bd3f
George Zubrienko <gzu@ecco.com>
2025-11-20 13:40:26 +0100

    WIP

--- src/main/scala/services/blobsource/readers/listing/BlobListingJsonSource.scala
+++ src/main/scala/services/blobsource/readers/listing/BlobListingJsonSource.scala
@@ -2,27 +2,19 @@ package com.sneaksanddata.arcane.framework
 package services.blobsource.readers.listing
 
 import models.batches.BlobBatchCommons
-import models.schemas.*
+import models.schemas.{ArcaneSchema, DataRow, given_CanAdd_ArcaneSchema}
 import services.base.SchemaProvider
 import services.blobsource.readers.BlobSourceReader
+import services.iceberg.given_Conversion_AvroSchema_ArcaneSchema
 import services.iceberg.interop.JsonScanner
-import services.iceberg.{
-  given_Conversion_AvroGenericRecord_DataRow,
-  given_Conversion_AvroSchema_ArcaneSchema,
-  given_Conversion_AvroType_ArcaneType
-}
 import services.storage.base.BlobStorageReader
 import services.storage.models.base.BlobPath
-import services.storage.models.s3.S3StoragePath
 
 import org.apache.avro.Schema as AvroSchema
-import org.apache.iceberg.avro.AvroSchemaUtil
-import zio.stream.ZStream
+import zio.stream.{ZSink, ZStream}
 import zio.{Task, ZIO}
 
-import java.security.MessageDigest
-import java.time.Duration
-import java.util.Base64
+import java.time.{Duration, OffsetDateTime}
 
 class BlobListingJsonSource[PathType <: BlobPath](
                                                    sourcePath: PathType,
@@ -35,12 +27,12 @@ class BlobListingJsonSource[PathType <: BlobPath](
 
   override type OutputRow = DataRow
   
-  private def getAvroSchema: Task[AvroSchema] = for
+  private def sourceSchema: Task[AvroSchema] = for
      parser <- ZIO.succeed(org.apache.avro.Schema.Parser())
   yield parser.parse(avroSchemaString)
 
   override def getSchema: Task[SchemaType] = for 
-      arcaneSchema <- getAvroSchema.map(implicitly)
+      arcaneSchema <- sourceSchema.map(implicitly)
   yield arcaneSchema ++ Seq(BlobBatchCommons.versionField)
 
   /** Gets an empty schema.
@@ -50,15 +42,27 @@ class BlobListingJsonSource[PathType <: BlobPath](
     */
   override def empty: SchemaType = ArcaneSchema.empty()
 
-  override def getChanges(startFrom: Long): ZStream[Any, Throwable, (OutputRow, Long)] = for
+  override def getChanges(startFrom: Long): ZStream[Any, Throwable, (DataRow, Long)] = for
     sourceFile <- reader.streamPrefixes(sourcePath).filter(_.createdOn.getOrElse(0L) >= startFrom)
     downloadedFile <- ZStream.fromZIO(
       reader.downloadBlob(s"${sourcePath.protocol}://${sourceFile.name}", tempStoragePath)
     )
-    scanner <- ZStream.fromZIO(ZIO.attempt(JsonScanner(downloadedFile)))
-    row     <- scanner.getRows.map(BlobBatchCommons.enrichBatchRow(_, sourceFile.createdOn.getOrElse(0), primaryKeys))
+    schema <- ZStream.fromZIO(sourceSchema)
+    scanner <- ZStream.succeed(JsonScanner(downloadedFile, schema))
+    row <- scanner.getRows.map(BlobBatchCommons.enrichBatchRow(_, sourceFile.createdOn.getOrElse(0), primaryKeys))
   yield (row, sourceFile.createdOn.getOrElse(0L))
 
-  override def getStartFrom(lookBackInterval: Duration): Task[Long] = ZIO.succeed(0)
+  // Listing readers do not support versioned streams, since they do not keep track of which file has been or not been processed
+  // thus they always act like they lookback until beginning of time
+  override def getStartFrom(lookBackInterval: Duration): Task[Long] = ZIO.succeed(
+    OffsetDateTime
+      .now()
+      .minus(lookBackInterval)
+      .toInstant
+      .toEpochMilli / 1000
+  )
 
-  override def getLatestVersion: Task[Long] = ZIO.succeed(0)
+  override def getLatestVersion: Task[Long] = reader
+    .streamPrefixes(sourcePath)
+    .map(_.createdOn.getOrElse(0L))
+    .run(ZSink.foldLeft(0L)((e, agg) => if (e > agg) e else agg))
--- src/main/scala/services/iceberg/interop/JsonScanner.scala
+++ src/main/scala/services/iceberg/interop/JsonScanner.scala
@@ -1,20 +1,22 @@
 package com.sneaksanddata.arcane.framework
 package services.iceberg.interop
 
+import logging.ZIOLogAnnotations.zlog
 import models.schemas.{ArcaneType, DataCell, DataRow}
+import services.iceberg.base.BlobScanner
 import services.iceberg.{given_Conversion_AvroGenericRecord_DataRow, given_Conversion_AvroType_ArcaneType}
 
 import org.apache.avro.data.Json
 import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
 import org.apache.avro.io.DecoderFactory
 import org.apache.iceberg.Schema
-import zio.Task
 import zio.stream.{ZPipeline, ZStream}
+import zio.{Task, ZIO}
 
 import java.io.File
 import scala.jdk.CollectionConverters.*
 
-class JsonScanner(schema: org.apache.avro.Schema, filePath: String):
+class JsonScanner(schema: org.apache.avro.Schema, filePath: String) extends BlobScanner:
   private val reader = GenericDatumReader[GenericRecord](schema)
 
   private def parseJsonLine(line: String): GenericRecord =
@@ -22,9 +24,17 @@ class JsonScanner(schema: org.apache.avro.Schema, filePath: String):
     reader.read(null, decoder)
 
 
-  def getRows: ZStream[Any, Throwable, DataRow] = ZStream.fromFileName(filePath)
+  override protected def getRowStream: ZStream[Any, Throwable, DataRow] = ZStream.fromFileName(filePath)
     .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines) // assume each line a JSON object
     .map(parseJsonLine)
+
+  override def cleanup: Task[Unit] = for
+    file <- ZIO.succeed(new File(filePath))
+    _    <- zlog("Row stream finished for file %s, deleting", filePath)
+    _ <- ZIO
+      .attemptBlockingIO(file.delete())
+      .orDie // require deletion to succeed, to avoid risking filling up the temp storage
+  yield ()
 
 object JsonScanner:
   def apply(path: String, schema: org.apache.avro.Schema): JsonScanner = new JsonScanner(
