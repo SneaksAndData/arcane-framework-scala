60606a2d496240708d4883921b4cfda6a79cdd0b
George Zubrienko <gzu@ecco.com>
2026-01-26 10:46:20 +0100

    WIP age metrics

--- src/main/scala/services/streaming/processors/batch_processors/WatermarkProcessingExtensions.scala
+++ src/main/scala/services/streaming/processors/batch_processors/WatermarkProcessingExtensions.scala
@@ -6,27 +6,12 @@ import models.batches.StagedBatch
 import services.iceberg.IcebergS3CatalogWriter
 import services.metrics.DeclaredMetrics
 import services.streaming.base.OffsetDateTimeRW.rw
-import services.streaming.base.{JsonWatermark, SourceWatermark}
+import services.streaming.base.{JsonWatermark, SourceWatermark, TimestampOnlyWatermark}
 
 import upickle.default.*
 import zio.ZIO
 
 import java.time.{Duration, OffsetDateTime}
-
-case class TimestampOnlyWatermark(timestamp: OffsetDateTime) extends JsonWatermark:
-  override def toJson: String = upickle.write(this)
-
-  /**
-   * Age of this watermark at the current moment of time, similar to SourceWatermark
-   *
-   * @return
-   */
-  def age: Long = Duration.between(timestamp, OffsetDateTime.now()).toSeconds
-  
-object  TimestampOnlyWatermark:
-  implicit val rw: ReadWriter[TimestampOnlyWatermark] = macroRW
-
-  def fromJson(value: String): TimestampOnlyWatermark = upickle.read(value)
 
 object WatermarkProcessingExtensions:
   extension (batch: StagedBatch)
