fa9cfdbd7efd4e28a234ef1c3d0d7e51abcb578d
George Zubrienko <gzu@ecco.com>
2026-01-29 15:44:35 +0100

    Also do not use batch name in Watermark logging

--- src/main/scala/services/streaming/processors/batch_processors/WatermarkProcessingExtensions.scala
+++ src/main/scala/services/streaming/processors/batch_processors/WatermarkProcessingExtensions.scala
@@ -3,16 +3,11 @@ package services.streaming.processors.batch_processors
 
 import logging.ZIOLogAnnotations.zlog
 import models.batches.StagedBatch
-import services.iceberg.IcebergS3CatalogWriter
+import services.iceberg.base.TablePropertyManager
 import services.metrics.DeclaredMetrics
-import services.streaming.base.OffsetDateTimeRW.rw
-import services.streaming.base.{JsonWatermark, SourceWatermark, TimestampOnlyWatermark}
+import services.streaming.base.TimestampOnlyWatermark
 
-import com.sneaksanddata.arcane.framework.services.iceberg.base.TablePropertyManager
-import upickle.default.*
 import zio.ZIO
-
-import java.time.{Duration, OffsetDateTime}
 
 object WatermarkProcessingExtensions:
   extension (batch: StagedBatch)
@@ -25,8 +20,7 @@ object WatermarkProcessingExtensions:
           for
             watermark <- ZIO.attempt(batch.completedWatermarkValue.get)
             _ <- zlog(
-              "Batch %s completed stream from watermark %s, will update target watermark",
-              batch.name,
+              "Changes associated with watermark %s has finished streaming, will update target",
               watermark
             )
             previousWatermark <- propertyManager.getProperty(targetName, "comment")
