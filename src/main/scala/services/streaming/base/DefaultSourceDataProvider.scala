e164e3f77c965b5be2771c1c6dc4a9ae3e5173a3
George Zubrienko <gzu@ecco.com>
2026-03-04 15:16:32 +0100

    Introduce standard source data provider impl

---
+++ src/main/scala/services/streaming/base/DefaultSourceDataProvider.scala
@@ -0,0 +1,78 @@
+package com.sneaksanddata.arcane.framework
+package services.streaming.base
+
+import logging.ZIOLogAnnotations.zlog
+import models.schemas.{DataRow, JsonWatermarkRow}
+import models.settings.backfill.BackfillSettings
+import models.settings.sink.SinkSettings
+import services.iceberg.base.SinkPropertyManager
+import services.streaming.throughput.base.ThroughputShaperBuilder
+
+import upickle.ReadWriter
+import zio.stream.ZStream
+import zio.{Task, ZIO}
+
+import java.time.OffsetDateTime
+
+/** Default implementations for source data emitter used by StreamDataProvider
+  * @tparam WatermarkType
+  *   Watermark implementation for the source
+  */
+abstract class DefaultSourceDataProvider[WatermarkType <: SourceWatermark[String] & JsonWatermark](
+    sinkPropertyManager: SinkPropertyManager,
+    sinkSettings: SinkSettings,
+    backfillSettings: BackfillSettings,
+    throughputShaperBuilder: ThroughputShaperBuilder
+)(implicit rw: ReadWriter[WatermarkType])
+    extends VersionedDataProvider[WatermarkType, DataRow]
+    with BackfillDataProvider[DataRow]:
+
+  private val throughputShaper = throughputShaperBuilder.build
+
+  final override def requestChanges(
+      previousVersion: WatermarkType,
+      nextVersion: WatermarkType
+  ): ZStream[Any, Throwable, DataRow] = throughputShaper
+    .shapeStream(changeStream(previousVersion, nextVersion))
+    .concat(ZStream.succeed(JsonWatermarkRow(nextVersion)))
+
+  /** Implements data streaming logic for public `requestChanges`
+    * @param previousVersion
+    *   Previous watermark
+    * @param nextVersion
+    *   Current watermark
+    * @return
+    */
+  protected def changeStream(
+      previousVersion: WatermarkType,
+      nextVersion: WatermarkType
+  ): ZStream[Any, Throwable, DataRow]
+
+  /** Evaluates watermark to be used when evaluating current snapshot version at the start of a backfill process
+    * @param startTime
+    * @return
+    */
+  protected def getBackfillStartWatermark(startTime: Option[OffsetDateTime]): WatermarkType
+
+  protected def backfillStream(backfillStartDate: Option[OffsetDateTime]): ZStream[Any, Throwable, DataRow]
+
+  final override def requestBackfill: ZStream[Any, Throwable, DataRow] = ZStream
+    .fromZIO(getCurrentVersion(getBackfillStartWatermark(backfillSettings.backfillStartDate)))
+    .flatMap(version =>
+      throughputShaper
+        .shapeStream(backfillStream(backfillSettings.backfillStartDate))
+        .concat(ZStream.succeed(JsonWatermarkRow(version)))
+    )
+
+  override def firstVersion: Task[WatermarkType] = for
+    watermarkString <- sinkPropertyManager.getProperty(sinkSettings.targetTableNameParts.Name, "comment")
+    _               <- zlog("Current watermark value on %s is '%s'", sinkSettings.targetTableFullName, watermarkString)
+    watermark <- ZIO
+      .attempt(upickle.read(watermarkString))
+      .orDieWith(e =>
+        new Throwable(
+          s"Target contains invalid watermark: '$watermarkString'. Please run a backfill or update the watermark manually via COMMENT ON statement",
+          e
+        )
+      )
+  yield watermark
