ebac9a18b23baf90f4cc51fe176d26958ca7ad83
George Zubrienko <gzu@ecco.com>
2026-03-10 11:44:11 +0100

    Get new plugin context impl-ready

--- src/main/scala/models/app/DefaultPluginStreamContext.scala
+++ src/main/scala/models/app/DefaultPluginStreamContext.scala
@@ -6,12 +6,17 @@ import models.settings.sink.DefaultSinkSettings
 import models.settings.staging.DefaultStagingSettings
 import models.settings.streaming.{DefaultStreamModeSettings, DefaultThroughputSettings}
 
+/** Baseline implementation of a `PluginStreamContext`. Provides default, serializable configuration sections for
+  * settings that compose foundational framework components.
+  *
+  * Plugin developers must provide a concrete implementation of this class by defining `override val source` and related
+  * implementation of `SourceSettings` Use `derives ReadWriter` on the concrete implementation to enable automatic
+  * context deserialization.
+  */
 abstract class DefaultPluginStreamContext(
     final override val observability: DefaultObservabilitySettings,
     final override val staging: DefaultStagingSettings,
     final override val streamMode: DefaultStreamModeSettings,
     final override val sink: DefaultSinkSettings,
     final override val throughput: DefaultThroughputSettings
-) extends PluginStreamContext:
-
-  override def merge(other: Option[PluginStreamContext]): PluginStreamContext = ???
+) extends PluginStreamContext
