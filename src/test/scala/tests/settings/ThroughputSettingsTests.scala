d9964a8629251a24e69887d469cf575a584fb5e3
George Zubrienko <gzu@ecco.com>
2026-03-11 20:25:31 +0100

    Add one more test for good measure

--- src/test/scala/tests/settings/ThroughputSettingsTests.scala
+++ src/test/scala/tests/settings/ThroughputSettingsTests.scala
@@ -1,14 +1,14 @@
 package com.sneaksanddata.arcane.framework
 package tests.settings
 
-import models.settings.streaming.{DefaultThroughputSettings, MemoryBound, ThroughputShaperImplSettings}
+import models.settings.streaming.{DefaultThroughputSettings, MemoryBound, Static, ThroughputShaperImplSettings}
 
 import org.scalatest.Inspectors.forAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.should
-
 import upickle.default.*
+
 import java.time.Duration
 
 class ThroughputSettingsTests extends AnyFlatSpec with Matchers:
@@ -38,6 +38,19 @@ class ThroughputSettingsTests extends AnyFlatSpec with Matchers:
         advisedChunksBurst = 1
       ),
       """{"shaperImpl":{"memoryBound":{"meanStringTypeSizeEstimate":1,"meanObjectTypeSizeEstimate":1,"burstEstimateDivisionFactor":1,"rateEstimateDivisionFactor":1,"chunkCostScale":1,"chunkCostMax":1,"tableRowCountWeight":1,"tableSizeWeight":1,"tableSizeScaleFactor":1},"static":null},"advisedRatePeriod":"1 second","advisedChunksBurst":1,"advisedChunkSize":1,"advisedRateChunks":1}"""
+    ),
+    (
+      DefaultThroughputSettings(
+        ThroughputShaperImplSettings(
+          memoryBound = None,
+          static = Some(Static())
+        ),
+        advisedRatePeriod = Duration.ofSeconds(1),
+        advisedChunkSize = 1,
+        advisedRateChunks = 1,
+        advisedChunksBurst = 1
+      ),
+      """{"shaperImpl":{"memoryBound":null,"static":{}},"advisedRatePeriod":"1 second","advisedChunksBurst":1,"advisedChunkSize":1,"advisedRateChunks":1}"""
     )
   )
 
