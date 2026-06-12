2c284658721db5161f7cf6928d1c87f26ee25bda
George Zubrienko <zubrienkog@gmail.com>
2026-06-12 15:02:07 +0200

    Fixes for SynapseLink (#448)

--- src/main/scala/models/queries/backfill/synapse/SynapseLinkShardCommitQuery.scala
+++ src/main/scala/models/queries/backfill/synapse/SynapseLinkShardCommitQuery.scala
@@ -10,7 +10,7 @@ type SynapseLinkShardCommitQuery = OverwriteReplaceQuery
 object SynapseLinkShardCommitQuery:
   def apply(targetName: String, combineTableName: String): OverwriteQuery = OverwriteReplaceQuery(
     sourceQuery = s"""SELECT * FROM (
-                     | SELECT * FROM $combineTableName WHERE ORDER BY ROW_NUMBER() OVER (PARTITION BY ${MergeKeyField.name} ORDER BY versionnumber DESC) FETCH FIRST 1 ROWS WITH TIES
+                     | SELECT * FROM $combineTableName WHERE COALESCE(IsDelete, false) = false ORDER BY ROW_NUMBER() OVER (PARTITION BY ${MergeKeyField.name} ORDER BY versionnumber DESC) FETCH FIRST 1 ROWS WITH TIES
                      |)""".stripMargin,
     targetName = targetName,
     tablePropertiesSettings = EmptyTablePropertiesSettings
