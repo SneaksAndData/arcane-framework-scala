2a5ca4a5af3daa646ca7f15971d0950788ecd847
Timon <tivi@ecco.com>
2026-06-23 14:59:05 +0200

    format

--- src/test/scala/tests/dynamodb/DynamodbSourceTests.scala
+++ src/test/scala/tests/dynamodb/DynamodbSourceTests.scala
@@ -19,34 +19,51 @@ import scala.language.postfixOps
 import tests.shared.{IcebergUtil, TestDynamicSinkSettings, TestSinkSettings}
 
 object DynamoTestServices:
-  val access_kid = "test"
+  val access_kid    = "test"
   val access_secret = "test"
 
   def getClient: Task[DynamoDbClient] =
-    ZIO.attempt(DynamoDbClient.builder().endpointOverride(URI.create("http://localhost:8000")).region(Region.US_EAST_1).credentialsProvider(
-      StaticCredentialsProvider.create(AwsBasicCredentials.create(access_kid, access_secret))
-    ).build())
+    ZIO.attempt(
+      DynamoDbClient
+        .builder()
+        .endpointOverride(URI.create("http://localhost:8000"))
+        .region(Region.US_EAST_1)
+        .credentialsProvider(
+          StaticCredentialsProvider.create(AwsBasicCredentials.create(access_kid, access_secret))
+        )
+        .build()
+    )
 
   def createTable(tableName: String, client: DynamoDbClient): Task[CreateTableResponse] = for {
-    req = CreateTableRequest.builder().tableName(tableName).keySchema(
-      KeySchemaElement.builder().attributeName("userId").keyType(KeyType.HASH).build()
-    ).attributeDefinitions(
-      AttributeDefinition.builder()
-        .attributeName("userId")
-        .attributeType(ScalarAttributeType.S)
-        .build()
-    ).provisionedThroughput(
-      ProvisionedThroughput.builder()
-        .readCapacityUnits(5L)
-        .writeCapacityUnits(5L)
-        .build()
-    ).build()
+    req = CreateTableRequest
+      .builder()
+      .tableName(tableName)
+      .keySchema(
+        KeySchemaElement.builder().attributeName("userId").keyType(KeyType.HASH).build()
+      )
+      .attributeDefinitions(
+        AttributeDefinition
+          .builder()
+          .attributeName("userId")
+          .attributeType(ScalarAttributeType.S)
+          .build()
+      )
+      .provisionedThroughput(
+        ProvisionedThroughput
+          .builder()
+          .readCapacityUnits(5L)
+          .writeCapacityUnits(5L)
+          .build()
+      )
+      .build()
     r <- ZIO.attemptBlocking(client.createTable(req))
   } yield r
 
-  def listTables(client: DynamoDbClient): Task[List[String]] = ZIO.attemptBlocking(
-    client.listTables(ListTablesRequest.builder().build())
-  ).map(_.tableNames().toArray.toList.map(_.toString))
+  def listTables(client: DynamoDbClient): Task[List[String]] = ZIO
+    .attemptBlocking(
+      client.listTables(ListTablesRequest.builder().build())
+    )
+    .map(_.tableNames().toArray.toList.map(_.toString))
 
   def deleteTable(client: DynamoDbClient, tableName: String): Task[DeleteTableResponse] =
     ZIO.attemptBlocking(client.deleteTable(DeleteTableRequest.builder().tableName(tableName).build()))
@@ -56,25 +73,29 @@ object DynamodbSourceTests extends ZIOSpecDefault:
 
   override def spec: Spec[TestEnvironment & Scope, Any] = suite("DynamodbConnectionTests")(
     test("DynamoDB has changes") {
-      val tableName = "testTable"
+      val tableName   = "testTable"
       val icebergUtil = IcebergUtil(TestDynamicSinkSettings(tableName).icebergCatalog)
       for {
         client <- DynamoTestServices.getClient
-        result      <- ZIO.acquireReleaseWith(
+        result <- ZIO.acquireReleaseWith(
           DynamoTestServices.createTable(tableName, client)
-        )(_ => DynamoTestServices.deleteTable(client, tableName).orDie)
-          { resp =>
+        )(_ => DynamoTestServices.deleteTable(client, tableName).orDie) { resp =>
           for {
-            _ <- Console.printLine(resp.toString)
-            tables <- DynamoTestServices.listTables(client)
-            _ <- Console.printLine(s"----------------------\n$tables")
+            _                   <- Console.printLine(resp.toString)
+            tables              <- DynamoTestServices.listTables(client)
+            _                   <- Console.printLine(s"----------------------\n$tables")
             sinkPropertyManager <- icebergUtil.getSinkTablePropertyManager
-            pushStreamSource <- ZIO.succeed(PushStreamingSource(
-              "testTable",
-              "testTable",
-              dynamodbClient = client, sinkPropertyManager = sinkPropertyManager
-            ))
-            changes <- pushStreamSource.hasRows(previousVersion = PushStreamWatermark(OffsetDateTime.ofInstant(Instant.now(), ZoneOffset.UTC)))
+            pushStreamSource <- ZIO.succeed(
+              PushStreamingSource(
+                "testTable",
+                "testTable",
+                dynamodbClient = client,
+                sinkPropertyManager = sinkPropertyManager
+              )
+            )
+            changes <- pushStreamSource.hasRows(previousVersion =
+              PushStreamWatermark(OffsetDateTime.ofInstant(Instant.now(), ZoneOffset.UTC))
+            )
           } yield assertTrue(tables.contains(tableName)) && assertTrue(changes)
         }
       } yield result
