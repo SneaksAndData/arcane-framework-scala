47705b6fb0dd22e6f12c103f7b68113fba62bab1
Timon <tivi@ecco.com>
2026-06-29 11:52:58 +0200

    wip

--- src/main/scala/models/settings/sources/pushstream/PushStreamSourceSettings.scala
+++ src/main/scala/models/settings/sources/pushstream/PushStreamSourceSettings.scala
@@ -4,9 +4,11 @@ package models.settings.sources.pushstream
 import upickle.ReadWriter
 import upickle.implicits.key
 
+import models.settings.sources.SourceSettings
+
 /** Microsoft SQL Server database connection settings
   */
-trait PushStreamSourceSettings:
+trait PushStreamSourceSettings extends SourceSettings:
   val sourceTableName: String
   // TODO: table names should be iceberg compliant {warehouse}.{namespace}.{tablename}
   val targetTableName: String
