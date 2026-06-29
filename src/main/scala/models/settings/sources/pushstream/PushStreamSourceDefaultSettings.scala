369d924d5a8f6e095165fd1dd114f5987726d3af
Timon <tivi@ecco.com>
2026-06-29 10:51:25 +0200

    wip

---
+++ src/main/scala/models/settings/sources/pushstream/PushStreamSourceDefaultSettings.scala
@@ -0,0 +1,18 @@
+package com.sneaksanddata.arcane.framework
+package models.settings.sources.pushstream
+
+package com.sneaksanddata.arcane.framework
+package models.settings.sources.pushstream
+
+import upickle.ReadWriter
+
+case class DefaultPushStreamSourceSettings(
+    override val sourceTableName: String,
+    override val targetTableName: String,
+    override val primaryKeyFieldName: String,
+    override val primaryKeyValue: String,
+    override val watermarkFieldName: String,
+    override val region: String,
+    override val tableName: String,
+    override val endpoint: Option[String]
+) extends PushStreamSourceSettings derives ReadWriter
