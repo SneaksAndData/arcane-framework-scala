d2cb36fd365c0a2d826efb1f88f5361c1cfef0f0
George Zubrienko <gzu@ecco.com>
2026-07-15 11:36:15 +0200

    limit shard sizes a bit

--- src/main/scala/services/blobsource/readers/listing/BlobListingStreamingSource.scala
+++ src/main/scala/services/blobsource/readers/listing/BlobListingStreamingSource.scala
@@ -78,8 +78,8 @@ abstract class BlobListingStreamingSource[PathType <: BlobPath](
     sample <- getEligibleFiles(rangeStart, rangeEnd).take(1000).runCollect
     // if file size cannot be determined, assume 100kb
     avgFileSize <- ZIO.succeed(sample.map(_.contentLength.getOrElse(1024L * 1024L * 100L)).sum / sample.size)
-    _           <- zlog("Average file size for shards: %s, max shard size is 100Mib", avgFileSize.toString)
-  yield (100L * 1024L * 1024L * 1024L / avgFileSize).toInt
+    _           <- zlog("Average file size for shards: %s bytes, max shard size is 100Mib", avgFileSize.toString)
+  yield Seq((100L * 1024L * 1024L * 1024L / avgFileSize).toInt, 10000).min
 
   final override def getShards(
       rangeStart: BlobSourceWatermark,
