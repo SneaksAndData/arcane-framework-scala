5a8d3f9be4a58338e4f71ced54a3c4cf6d7a3df4
Timon <tivi@ecco.com>
2026-08-25 15:55:48 +0200

    feat: add pullstream layer assembly

---
+++ src/main/scala/plugins/pullstream/Service.scala
@@ -0,0 +1,56 @@
+package com.sneaksanddata.arcane.framework
+package plugins.pullstream
+
+import models.app.PluginStreamContext
+import services.backfill.DefaultBackfillStateManager
+import services.backfill.base.{
+  BackfillStateManager,
+  BackfillStreamDataProvider,
+  ShardFactory,
+  ShardedBackfillStreamDataProvider
+}
+import services.iceberg.base.{SinkPropertyManager, StagingEntityManager, StagingPropertyManager}
+import services.metrics.DeclaredMetrics
+import services.naming.NameGenerator
+import services.pullstream.backfill.{
+  NoopBackfillStreamDataProvider,
+  NoopShardFactory,
+  NoopShardedBackfillStreamDataProvider
+}
+import services.pullstream.{
+  PullStreamSourceDataProvider,
+  PullStreamStagedBatchFactory,
+  PullStreamStreamingDataProvider,
+  PullStreamingSource
+}
+import services.streaming.base.StreamDataProvider
+import services.streaming.batching.StagedBatchFactory
+import services.streaming.throughput.base.ThroughputShaperBuilder
+
+import zio.ZLayer
+
+type PullStreamRequiredServices = SinkPropertyManager & StagingEntityManager & StagingPropertyManager & NameGenerator &
+  DeclaredMetrics
+type PullStreamProvidedServices = StagedBatchFactory & ShardFactory & ShardedBackfillStreamDataProvider &
+  StreamDataProvider & BackfillStreamDataProvider & BackfillStateManager
+
+object Services:
+  lazy val sourceLayer: ZLayer[
+    PullStreamRequiredServices & PullStreamingSource & PluginStreamContext,
+    Throwable,
+    PullStreamProvidedServices
+  ] = ZLayer.makeSome[
+    PullStreamRequiredServices & PullStreamingSource & PluginStreamContext,
+    PullStreamProvidedServices
+  ](
+    PullStreamSourceDataProvider.layer,
+    PullStreamStreamingDataProvider.layer,
+    PullStreamStagedBatchFactory.layer,
+
+    // backfill and sharding are not supported by the pull stream plugin
+    NoopBackfillStreamDataProvider.layer,
+    NoopShardedBackfillStreamDataProvider.layer,
+    NoopShardFactory.layer,
+    ThroughputShaperBuilder.layer,
+    DefaultBackfillStateManager.layer
+  )
