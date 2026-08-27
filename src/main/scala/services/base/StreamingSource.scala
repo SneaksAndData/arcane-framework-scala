0f90e8fb0747735714d1544a40848804f315892f
Timon <tivi@ecco.com>
2026-08-27 09:34:03 +0200

    refact: remove stale comment

--- src/main/scala/services/base/StreamingSource.scala
+++ src/main/scala/services/base/StreamingSource.scala
@@ -5,4 +5,3 @@ import models.schemas.ArcaneSchema
 import services.streaming.base.{JsonWatermark, SourceWatermark}
 
 trait StreamingSource extends SchemaProvider[ArcaneSchema] with ShardProvider
-// TODO:  final override type SchemaType = ArcaneSchema
