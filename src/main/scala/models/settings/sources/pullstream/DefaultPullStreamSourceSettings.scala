3179c9c118e29129cebd43925d69b42bc3e35aee
Timon <tivi@ecco.com>
2026-08-28 09:57:17 +0200

    fix: add default None values to pull-stream source config

--- src/main/scala/models/settings/sources/pullstream/DefaultPullStreamSourceSettings.scala
+++ src/main/scala/models/settings/sources/pullstream/DefaultPullStreamSourceSettings.scala
@@ -7,9 +7,9 @@ case class DefaultPullStreamSourceSettings(
     override val pullIndexKey: String,
     override val pullIndexValue: String,
     override val versionFieldName: String,
-    override val pageSize: Option[Int],
+    override val pageSize: Option[Int] = None,
     override val region: String,
     override val tableName: String,
-    override val endpoint: Option[String],
-    override val jsonPointerExpression: Option[String]
+    override val endpoint: Option[String] = None,
+    override val jsonPointerExpression: Option[String] = None
 ) extends PullStreamSourceSettings derives ReadWriter
