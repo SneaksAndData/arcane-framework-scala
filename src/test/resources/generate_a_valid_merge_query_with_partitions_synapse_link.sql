9e26b40efb922903adeb748ea45a3649894ce242
Lukas Mikelionis <lumi@ecco.com>
2026-09-01 11:20:34 +0200

    use ARCANE_VERSION in merge queries

--- src/test/resources/generate_a_valid_merge_query_with_partitions_synapse_link.sql
+++ src/test/resources/generate_a_valid_merge_query_with_partitions_synapse_link.sql
@@ -1,16 +1,16 @@
 MERGE INTO test.table_a t_o
 USING (SELECT * FROM (
- SELECT * FROM test.staged_a ORDER BY ROW_NUMBER() OVER (PARTITION BY ARCANE_MERGE_KEY ORDER BY versionnumber DESC) FETCH FIRST 1 ROWS WITH TIES
+ SELECT * FROM test.staged_a ORDER BY ROW_NUMBER() OVER (PARTITION BY ARCANE_MERGE_KEY ORDER BY ARCANE_VERSION DESC) FETCH FIRST 1 ROWS WITH TIES
 )) t_s
 ON t_o.ARCANE_MERGE_KEY = t_s.ARCANE_MERGE_KEY AND (t_o.colA = t_s.colA OR t_s.colA IS NULL)
 WHEN MATCHED AND coalesce(t_s.IsDelete, false) = true THEN DELETE
-WHEN MATCHED AND coalesce(t_s.IsDelete, false) = false AND t_s.versionnumber > t_o.versionnumber THEN UPDATE SET
+WHEN MATCHED AND coalesce(t_s.IsDelete, false) = false AND t_s.ARCANE_VERSION > t_o.ARCANE_VERSION THEN UPDATE SET
  colA = t_s.colA,
 colB = t_s.colB,
 Id = t_s.Id,
-versionnumber = t_s.versionnumber
-WHEN NOT MATCHED AND coalesce(t_s.IsDelete, false) = false THEN INSERT (ARCANE_MERGE_KEY,colA,colB,Id,versionnumber) VALUES (t_s.ARCANE_MERGE_KEY,
+ARCANE_VERSION = t_s.ARCANE_VERSION
+WHEN NOT MATCHED AND coalesce(t_s.IsDelete, false) = false THEN INSERT (ARCANE_MERGE_KEY,colA,colB,Id,ARCANE_VERSION) VALUES (t_s.ARCANE_MERGE_KEY,
 t_s.colA,
 t_s.colB,
 t_s.Id,
-t_s.versionnumber)
+t_s.ARCANE_VERSION)
