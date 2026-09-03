aeccb9ec1186a4d8f54f52d82094380097a7cd41
Vitalii Savitskii <visa@ecco.com>
2026-09-03 10:42:28 +0200

    Build fix

--- src/main/scala/models/app/OverrideStreamContext.scala
+++ src/main/scala/models/app/OverrideStreamContext.scala
@@ -1,7 +1,6 @@
 package com.sneaksanddata.arcane.framework
 package models.app
 
-import models.settings.sources.OverrideStreamSourceSettings
 import models.settings.streaming.OverrideStreamModeSettings
 
 /** The stream mode that can be overridden by the stream override provided by the arcane operator with the environment
@@ -9,5 +8,3 @@ import models.settings.streaming.OverrideStreamModeSettings
   */
 trait OverrideStreamContext:
   val streamMode: Option[OverrideStreamModeSettings]
-
-  val source: Option[OverrideStreamSourceSettings]
