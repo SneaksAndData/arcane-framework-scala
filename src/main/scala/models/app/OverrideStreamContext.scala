fcfcea886149c3ca87f84b6601bb0a4e99d66d81
Vitalii Savitskii <visa@ecco.com>
2026-09-03 11:28:25 +0200

    Build fix

--- src/main/scala/models/app/OverrideStreamContext.scala
+++ src/main/scala/models/app/OverrideStreamContext.scala
@@ -1,10 +1,20 @@
 package com.sneaksanddata.arcane.framework
 package models.app
 
-import models.settings.streaming.OverrideStreamModeSettings
+import models.settings.streaming.{DefaultOverrideStreamModeSettings, OverrideStreamModeSettings}
+
+import upickle.ReadWriter
+import upickle.implicits.key
 
 /** The stream mode that can be overridden by the stream override provided by the arcane operator with the environment
   * variable.
   */
 trait OverrideStreamContext:
   val streamMode: Option[OverrideStreamModeSettings]
+
+/** The stream mode that can be overridden by the stream override provided by the arcane operator with the environment
+  * variable.
+  */
+case class DefaultOverrideStreamContext(
+    @key("streamMode") override val streamMode: Option[DefaultOverrideStreamModeSettings] = None
+) extends OverrideStreamContext derives ReadWriter
